64b2ef594327b8b1a047f925686dd2653c58686d
michaelborhlar <michealadefehinti09@gmail.com>
2025-05-22 17:12:29 +0100

    added simple intrest file

---
+++ python_introduction/simple_intrest.py
@@ -0,0 +1,7 @@
+#create variable for principal,rate and time
+principal = 1000
+rate = 0.05
+time = 3
+#Variable for simple intrest
+intrest = principal * rate * time
+print('The simple intrest is:', intrest)
