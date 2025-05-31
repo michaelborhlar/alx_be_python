0824dea93a463dd653263f3a60fe55d0218f5db3
michaelborhlar <michealadefehinti09@gmail.com>
2025-05-31 14:26:46 +0100

    Add daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,28 +1,28 @@
 task = str(input("Enter your task: "))
 
-task_priority = str(input("Priority (high/medium/low): "))
+priority = str(input("Priority (high/medium/low): "))
 
 time_bound = str(input("Is it time-bound? (yes/no): "))
 
 
-match task_priority:
+match priority:
     case 'high':
         if time_bound == "yes":
-            print(f"Reminder: '{task}' is a {task_priority} priority task that requires immediate attention today!")
+            print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
         else:
-            print(f"Note: '{task}' is a {task_priority} priority task. Consider completing it when you have free time.")
+            print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
             
         
     case 'medium':
         if time_bound == "yes":
-            print(f"Reminder: '{task}' is a {task_priority} priority task that requires immediate attention today!")
+            print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
         else:
-            print(f"Note: '{task}' is a {task_priority} priority task. Consider completing it when you have free time.")
+            print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
     case "low":
         if time_bound == "yes":
-            print(f"Reminder: '{task}' is a {task_priority} priority task that requires immediate attention today!")
+            print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
         else:   
-            print(f"Note: '{task}' is a {task_priority} priority task. Consider completing it when you have free time.")
+            print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
         
         
         
