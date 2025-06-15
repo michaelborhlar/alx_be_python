9cfac666b17afc9c6aad2b8ce451ab33d8160623
michaelborhlar <michealadefehinti09@gmail.com>
2025-06-15 19:43:14 +0100

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,6 +1,9 @@
 def safe_divide(numerator, denominator):
   try:
-    result = float(numerator/denominator)
+    num = float(numerator)
+    den = float(denominator)
+    result = num/den
+    
     print(f"The result of the division is {result}")
   except ZeroDivisionError:
     print("Error: Cannot divide by zero.")
