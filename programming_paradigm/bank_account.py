26c3c3f483cbee369254adfdf7f10836b4c24fb7
michaelborhlar <michealadefehinti09@gmail.com>
2025-06-15 19:28:53 +0100

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,4 +15,4 @@ class BankAccount:
         
     def  display_balance(self):
         
-        print(f"Current Balance: ${self.account_balance.2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
